ace1a494a04db58b9d02b615660c20cc2ee0e1ec
Daniel Sauvé <2496407+megametres@users.noreply.github.com>
2020-06-19 23:58:24 -0400

    Convert image to base64

---
+++ src/database/recipe_helper/image_converter.rs
@@ -0,0 +1,43 @@
+extern crate rustc_serialize;
+// use crate::error::*;
+
+use rustc_serialize::base64::{ToBase64, MIME};
+use rustc_serialize::hex::ToHex;
+
+pub fn get_file_type(hex: &str) -> &str {
+    match &hex[..8] {
+        r"ffd8ffe0" => "jpeg",
+        r"89504e47" => "png",
+        r"47494638" => "gif",
+        _ => panic!("invalid file type"),
+    }
+}
+
+pub fn imageurl_to_base64(image_url: &str) -> String {
+    let result = reqwest::blocking::get(image_url).unwrap().bytes().unwrap();
+
+    let base64 = result.to_base64(MIME);
+    let hex = result.to_hex();
+
+    format!(
+        "data:image/{};base64,{}",
+        get_file_type(&hex),
+        base64.replace("\r\n", "")
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //     // #[test]
+    //     fn test_load_image_png() {
+    //         let test_image = load_image("https://via.placeholder.com/5.png").unwrap();
+    //         assert_eq!(test_image.content_type, rocket::http::ContentType::PNG);
+    //     }
+    // #[test]
+    // fn test_load_image_jpg() {
+    //     let test_image = imageurl_to_base64("https://via.placeholder.com/5.jpg");
+
+    //     // assert_eq!(test_image.content_type, rocket::http::ContentType::JPEG);
+    // }
+}
