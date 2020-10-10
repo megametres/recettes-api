05e298b87cebe94999ddc1b3fdacadc53a2d06bb
Daniel Sauvé <megametres@users.noreply.github.com>
2020-10-09 21:05:34 -0400

    Convert unicode to utf-8

--- postgresql-init/seed.sql
+++ postgresql-init/seed.sql
@@ -55,15 +55,15 @@ CREATE TABLE "public"."how_to_step" (
 ) WITH (oids = false);
 
 INSERT INTO "how_to_step" ("id", "name") VALUES
-(1,	'Dans un bol, m\u00e9langer la farine, la poudre d\u2019amandes, la poudre \u00e0 p\u00e2te et le sel. R\u00e9server.'),
-(2,	'Dans un autre bol, cr\u00e9mer le beurre avec le sucre \u00e0 glacer, l\u2019eau, la vanille et l\u2019extrait d\u2019amandes au batteur \u00e9lectrique. \u00c0 basse vitesse ou \u00e0 la cuill\u00e8re de bois, incorporer les ingr\u00e9dients secs.'),
-(3,	'Sur un plan de travail, d\u00e9poser la p\u00e2te sur une feuille de papier parchemin ou de papier d\u2019aluminium. Former un rouleau d\u2019environ 5\u00a0cm (2 po) de diam\u00e8tre. Refermer le rouleau en tortillant les deux extr\u00e9mit\u00e9s du papier d\u2019aluminium. R\u00e9frig\u00e9rer environ 3 heures ou jusqu\u2019\u00e0 ce que la p\u00e2te soit tr\u00e8s ferme au toucher.'),
-(4,	'Placer la grille au centre du four. Pr\u00e9chauffer le four \u00e0 190\u00a0\u00b0C (375 \u00b0F). Tapisser deux plaques \u00e0 biscuits de papier parchemin.'),
-(5,	'D\u00e9baller et placer le rouleau sur un plan de travail. Couper en tranches d\u2019environ 1 cm (\u00bd po) d\u2019\u00e9paisseur et les r\u00e9partir sur les plaques. '),
-(6,	'Cuire au four, une plaque \u00e0 la fois, environ 12 minutes, ou jusqu\u2019\u00e0 ce que les biscuits soient l\u00e9g\u00e8rement dor\u00e9s. Laisser refroidir sur la plaque.'),
-(7,	'Pr\u00e9chauffer l\u2019eau au cuiseur de pr\u00e9cision \u00e0 68\u00a0\u00b0C (155\u00a0\u00b0F).'),
-(8,	'Dans un bol, verser le sel et le poivre sur la cuisse de canard. Frotter pendant 1 minute. Placer le tout dans un sac \u00e0 fermeture herm\u00e9tique avec le reste des ingr\u00e9dients.'),
-(9,	'Placer dans l\u2019eau pr\u00e9chauff\u00e9e. Cuire 36 heures. Ajouter de l\u2019eau, au besoin, pour s\u2019assurer que la viande soit toujours submerg\u00e9e.');
+(1,	'Dans un bol, mélanger la farine, la poudre d’amandes, la poudre à pâte et le sel. Réserver.'),
+(2,	'Dans un autre bol, crémer le beurre avec le sucre à glacer, l’eau, la vanille et l’extrait d’amandes au batteur électrique. À basse vitesse ou à la cuillère de bois, incorporer les ingrédients secs.'),
+(3,	'Sur un plan de travail, déposer la pâte sur une feuille de papier parchemin ou de papier d’aluminium. Former un rouleau d’environ 5 cm (2 po) de diamètre. Refermer le rouleau en tortillant les deux extrémités du papier d’aluminium. Réfrigérer environ 3 heures ou jusqu’à ce que la pâte soit très ferme au toucher.'),
+(4,	'Placer la grille au centre du four. Préchauffer le four à 190 °C (375 °F). Tapisser deux plaques à biscuits de papier parchemin.'),
+(5,	'Déballer et placer le rouleau sur un plan de travail. Couper en tranches d’environ 1 cm (½ po) d’épaisseur et les répartir sur les plaques. '),
+(6,	'Cuire au four, une plaque à la fois, environ 12 minutes, ou jusqu’à ce que les biscuits soient légèrement dorés. Laisser refroidir sur la plaque.'),
+(7,	'Préchauffer l’eau au cuiseur de précision à 68 °C (155 °F).'),
+(8,	'Dans un bol, verser le sel et le poivre sur la cuisse de canard. Frotter pendant 1 minute. Placer le tout dans un sac à fermeture hermétique avec le reste des ingrédients.'),
+(9,	'Placer dans l’eau préchauffée. Cuire 36 heures. Ajouter de l’eau, au besoin, pour s’assurer que la viande soit toujours submergée.');
 
 DROP TABLE IF EXISTS "ingredient";
 DROP SEQUENCE IF EXISTS ingredient_id_seq;
@@ -78,18 +78,18 @@ CREATE TABLE "public"."ingredient" (
 
 INSERT INTO "ingredient" ("id", "name") VALUES
 (1,	'375 ml (1 1/2 tasse) de farine tout usage non blanchie'),
-(2,	'250 ml (1 tasse) de poudre d\u2019amandes'),
-(3,	'5 ml (1 c. \u00e0 th\u00e9) de poudre \u00e0 p\u00e2te'),
-(4,	'1 ml (1/4 c. \u00e0 th\u00e9) de sel'),
-(5,	'180 ml (3/4 tasse) de beurre non sal\u00e9, ramolli'),
-(6,	'250 ml (1 tasse) de sucre \u00e0 glacer'),
-(7,	'15 ml (1 c. \u00e0 soupe) d\u2019eau froide'),
-(8,	'5 ml (1 c. \u00e0 th\u00e9) d\u2019extrait de vanille'),
-(9,	'1 ml (1/4 c. \u00e0 th\u00e9) d\u2019extrait d\u2019amande (facultatif)'),
-(10,	'5 ml (1 c. \u00e0 th\u00e9) de fleur de sel (ou de sel casher)"'),
-(11,	'1 ml (1/4 c. \u00e0 th\u00e9) de poivre moulu"'),
+(2,	'250 ml (1 tasse) de poudre d’amandes'),
+(3,	'5 ml (1 c. à thé) de poudre à pâte'),
+(4,	'1 ml (1/4 c. à thé) de sel'),
+(5,	'180 ml (3/4 tasse) de beurre non salé, ramolli'),
+(6,	'250 ml (1 tasse) de sucre à glacer'),
+(7,	'15 ml (1 c. à soupe) d’eau froide'),
+(8,	'5 ml (1 c. à thé) d’extrait de vanille'),
+(9,	'1 ml (1/4 c. à thé) d’extrait d’amande (facultatif)'),
+(10,	'5 ml (1 c. à thé) de fleur de sel (ou de sel casher)"'),
+(11,	'1 ml (1/4 c. à thé) de poivre moulu"'),
 (12,	'1 cuisse de canard"'),
-(13,	'1 gousse d\u2019ail, pel\u00e9e et l\u00e9g\u00e8rement \u00e9cras\u00e9e"'),
+(13,	'1 gousse d’ail, pelée et légèrement écrasée"'),
 (14,	'1 feuille de laurier"'),
 (15,	'2 branches de thym"');
 
@@ -105,11 +105,11 @@ CREATE TABLE "public"."keyword" (
 ) WITH (oids = false);
 
 INSERT INTO "keyword" ("id", "name") VALUES
-(1,	'recettes de No\u00ebl'),
-(2,	'desserts de No\u00ebl'),
-(3,	'biscuits de No\u00ebl'),
-(4,	'biscuits sabl\u00e9s au beurre'),
-(5,	'recettes de biscuits sabl\u00e9s au beurre'),
+(1,	'recettes de Noël'),
+(2,	'desserts de Noël'),
+(3,	'biscuits de Noël'),
+(4,	'biscuits sablés au beurre'),
+(5,	'recettes de biscuits sablés au beurre'),
 (6,	'biscuits'),
 (7,	'recettes de biscuits'),
 (8,	'sous vide'),
@@ -119,7 +119,7 @@ INSERT INTO "keyword" ("id", "name") VALUES
 (12,	'canard sous vide'),
 (13,	'recettes canard'),
 (14,	'canard'),
-(15,	'cuiseur de pr\u00e9cision'),
+(15,	'cuiseur de précision'),
 (16,	'thermocirculateur');
 
 DROP TABLE IF EXISTS "recipe";
@@ -143,8 +143,8 @@ CREATE TABLE "public"."recipe" (
 ) WITH (oids = false);
 
 INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time", "cook_time", "total_time", "recipe_yield", "description", "json_ld") VALUES
-(1,	'Biscuits au beurre r\u00e9frig\u00e9rateur',	1,	'Ricardocuisine',	'https://images.ricardocuisine.com/services/recipes/4934.jpg',	'PT20M',	'PT12M',	'PT32M',	'40 biscuits, environ',	'Recette de Ricardo de biscuits au beurre r\u00e9frig\u00e9rateur',	'{
-    "name": "Biscuits au beurre r\u00e9frig\u00e9rateur",
+(1,	'Biscuits au beurre réfrigérateur',	1,	'Ricardocuisine',	'https://images.ricardocuisine.com/services/recipes/4934.jpg',	'PT20M',	'PT12M',	'PT32M',	'40 biscuits, environ',	'Recette de Ricardo de biscuits au beurre réfrigérateur',	'{
+    "name": "Biscuits au beurre réfrigérateur",
     "author": {
         "@type": "Person",
         "name": "Ricardocuisine"
@@ -155,14 +155,14 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
     "cookTime": "PT12M",
     "recipeIngredient": [
         "375 ml (1 1/2 tasse) de farine tout usage non blanchie",
-        "250 ml (1 tasse) de poudre d\u2019amandes",
-        "5 ml (1 c. \u00e0 th\u00e9) de poudre \u00e0 p\u00e2te",
-        "1 ml (1/4 c. \u00e0 th\u00e9) de sel",
-        "180 ml (3/4 tasse) de beurre non sal\u00e9, ramolli",
-        "250 ml (1 tasse) de sucre \u00e0 glacer",
-        "15 ml (1 c. \u00e0 soupe) d\u2019eau froide",
-        "5 ml (1 c. \u00e0 th\u00e9) d\u2019extrait de vanille",
-        "1 ml (1/4 c. \u00e0 th\u00e9) d\u2019extrait d\u2019amande (facultatif)"
+        "250 ml (1 tasse) de poudre d’amandes",
+        "5 ml (1 c. à thé) de poudre à pâte",
+        "1 ml (1/4 c. à thé) de sel",
+        "180 ml (3/4 tasse) de beurre non salé, ramolli",
+        "250 ml (1 tasse) de sucre à glacer",
+        "15 ml (1 c. à soupe) d’eau froide",
+        "5 ml (1 c. à thé) d’extrait de vanille",
+        "1 ml (1/4 c. à thé) d’extrait d’amande (facultatif)"
     ],
     "recipeInstructions": [
         {
@@ -171,27 +171,27 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
             "itemListElement": [
                 {
                     "@type": "HowToStep",
-                    "text": "Dans un bol, m\u00e9langer la farine, la poudre d\u2019amandes, la poudre \u00e0 p\u00e2te et le sel. R\u00e9server."
+                    "text": "Dans un bol, mélanger la farine, la poudre d’amandes, la poudre à pâte et le sel. Réserver."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Dans un autre bol, cr\u00e9mer le beurre avec le sucre \u00e0 glacer, l\u2019eau, la vanille et l\u2019extrait d\u2019amandes au batteur \u00e9lectrique. \u00c0 basse vitesse ou \u00e0 la cuill\u00e8re de bois, incorporer les ingr\u00e9dients secs."
+                    "text": "Dans un autre bol, crémer le beurre avec le sucre à glacer, l’eau, la vanille et l’extrait d’amandes au batteur électrique. À basse vitesse ou à la cuillère de bois, incorporer les ingrédients secs."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Sur un plan de travail, d\u00e9poser la p\u00e2te sur une feuille de papier parchemin ou de papier d\u2019aluminium. Former un rouleau d\u2019environ 5\u00a0cm (2 po) de diam\u00e8tre. Refermer le rouleau en tortillant les deux extr\u00e9mit\u00e9s du papier d\u2019aluminium. R\u00e9frig\u00e9rer environ 3 heures ou jusqu\u2019\u00e0 ce que la p\u00e2te soit tr\u00e8s ferme au toucher."
+                    "text": "Sur un plan de travail, déposer la pâte sur une feuille de papier parchemin ou de papier d’aluminium. Former un rouleau d’environ 5 cm (2 po) de diamètre. Refermer le rouleau en tortillant les deux extrémités du papier d’aluminium. Réfrigérer environ 3 heures ou jusqu’à ce que la pâte soit très ferme au toucher."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Placer la grille au centre du four. Pr\u00e9chauffer le four \u00e0 190\u00a0\u00b0C (375 \u00b0F). Tapisser deux plaques \u00e0 biscuits de papier parchemin."
+                    "text": "Placer la grille au centre du four. Préchauffer le four à 190 °C (375 °F). Tapisser deux plaques à biscuits de papier parchemin."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "D\u00e9baller et placer le rouleau sur un plan de travail. Couper en tranches d\u2019environ 1 cm (\u00bd po) d\u2019\u00e9paisseur et les r\u00e9partir sur les plaques. "
+                    "text": "Déballer et placer le rouleau sur un plan de travail. Couper en tranches d’environ 1 cm (½ po) d’épaisseur et les répartir sur les plaques. "
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Cuire au four, une plaque \u00e0 la fois, environ 12 minutes, ou jusqu\u2019\u00e0 ce que les biscuits soient l\u00e9g\u00e8rement dor\u00e9s. Laisser refroidir sur la plaque."
+                    "text": "Cuire au four, une plaque à la fois, environ 12 minutes, ou jusqu’à ce que les biscuits soient légèrement dorés. Laisser refroidir sur la plaque."
                 }
             ]
         }
@@ -205,9 +205,9 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
         "worstRating": "1"
     },
     "nutrition": null,
-    "description": "Recette de Ricardo de biscuits au beurre r\u00e9frig\u00e9rateur",
+    "description": "Recette de Ricardo de biscuits au beurre réfrigérateur",
     "recipeCategory": "Desserts",
-    "keywords": "recettes de No\u00ebl, desserts de No\u00ebl, biscuits de No\u00ebl,  biscuits sabl\u00e9s au beurre, recettes de biscuits sabl\u00e9s au beurre, biscuits, recettes de biscuits",
+    "keywords": "recettes de Noël, desserts de Noël, biscuits de Noël,  biscuits sablés au beurre, recettes de biscuits sablés au beurre, biscuits, recettes de biscuits",
     "totalTime": "PT32M",
     "review": [],
     "video": [],
@@ -225,10 +225,10 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
     "prepTime": "PT10M",
     "cookTime": "PT36H",
     "recipeIngredient": [
-        "5 ml (1 c. \u00e0 th\u00e9) de fleur de sel (ou de sel casher)",
-        "1 ml (1/4 c. \u00e0 th\u00e9) de poivre moulu",
+        "5 ml (1 c. à thé) de fleur de sel (ou de sel casher)",
+        "1 ml (1/4 c. à thé) de poivre moulu",
         "1 cuisse de canard",
-        "1 gousse d\u2019ail, pel\u00e9e et l\u00e9g\u00e8rement \u00e9cras\u00e9e",
+        "1 gousse d’ail, pelée et légèrement écrasée",
         "1 feuille de laurier",
         "2 branches de thym"
     ],
@@ -239,15 +239,15 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
             "itemListElement": [
                 {
                     "@type": "HowToStep",
-                    "text": "Pr\u00e9chauffer l\u2019eau au cuiseur de pr\u00e9cision \u00e0 68\u00a0\u00b0C (155\u00a0\u00b0F)."
+                    "text": "Préchauffer l’eau au cuiseur de précision à 68 °C (155 °F)."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Dans un bol, verser le sel et le poivre sur la cuisse de canard. Frotter pendant 1 minute. Placer le tout dans un sac \u00e0 fermeture herm\u00e9tique avec le reste des ingr\u00e9dients."
+                    "text": "Dans un bol, verser le sel et le poivre sur la cuisse de canard. Frotter pendant 1 minute. Placer le tout dans un sac à fermeture hermétique avec le reste des ingrédients."
                 },
                 {
                     "@type": "HowToStep",
-                    "text": "Placer dans l\u2019eau pr\u00e9chauff\u00e9e. Cuire 36 heures. Ajouter de l\u2019eau, au besoin, pour s\u2019assurer que la viande soit toujours submerg\u00e9e."
+                    "text": "Placer dans l’eau préchauffée. Cuire 36 heures. Ajouter de l’eau, au besoin, pour s’assurer que la viande soit toujours submergée."
                 }
             ]
         }
@@ -263,7 +263,7 @@ INSERT INTO "recipe" ("id", "name", "category_id", "author", "image", "prep_time
     "nutrition": null,
     "description": "Confit de canard sous vide | RICARDO",
     "recipeCategory": "Plats principaux",
-    "keywords": "sous vide, recettes avec sous vide, recettes sous vide, cuisiner au sous vide, canard sous vide, recettes canard, canard, cuiseur de pr\u00e9cision, thermocirculateur",
+    "keywords": "sous vide, recettes avec sous vide, recettes sous vide, cuisiner au sous vide, canard sous vide, recettes canard, canard, cuiseur de précision, thermocirculateur",
     "totalTime": "PT36H10M",
     "review": [],
     "video": [],
